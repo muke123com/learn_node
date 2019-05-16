74dc81c16d698bcb6e5d6e979cae79fcf719b814
mukeke <mukeke@quandashi.com>
2019-05-16 19:56:38 +0800

    spider

--- models/spider.js
+++ models/spider.js
@@ -8,8 +8,8 @@ let spiderModel = {};
 let table = 'm_';
 
 spiderModel.getData = async () => {
-    let url = "https://graphql.epicgames.com/graphql";
-    // let url = "http://localhost:3000/test";
+    // let url = "https://graphql.epicgames.com/graphql";
+    let url = "http://localhost:3000/test";
     let query = "";
     let data = {};
     data['query'] = query;
@@ -19,7 +19,7 @@ spiderModel.getData = async () => {
         url: url,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-            "cookie": "EPIC_SESSION_GRAPHQL=XQueGURVL4P4TpLxuYQmyg.pH_qoEEiH1IyWFIPOUJjjfpGVzNXp6lNfW0fRJm4bPGB-v-I-Wh8OmvAwyg90cwb.1557990060207.86400000.jZLLpyzeGVSHV3nwnrAPJgMytAOH_nF7SMiP7qpQps4"
+            'Host': "graphql.epicgames.com",
         },
         form: data
     };
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ const router = require('koa-router')()
 
 const spiderModel = require('../models/spider');
 
+router.prefix('/index');
+
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
 })
 
-router.get('/getData', async (ctx, next) => {
+router.post('/getData', async (ctx, next) => {
   let data = await spiderModel.getData();
   ctx.body = data;
 })
