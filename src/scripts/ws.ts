9b3fe876c8dcb1fb07147f052835ccb5658c7be6
muke123com <muke123com@163.com>
2019-06-13 20:29:16 +0800

    wx

--- src/scripts/ws.ts
+++ src/scripts/ws.ts
@@ -10,22 +10,43 @@ let socketSet = new Set<any>();
 // 连接
 wsServer.on('connection', websocket => {
     socketSet.add(websocket)
+    websocket.on('open', () => {
+        websocket.send("open")
+    })
+    
+    websocket.on('message', function incoming(data) {
+        websocket.send(data);
+    });
 });
+
+let socketList: any[] = Array.from(socketSet);
+socketList.forEach(ws => {
+    if (ws.readyState == 1) {
+        ws.on('open', () => {
+            ws.send("open")
+        })
+        ws.on('message', function incoming(data: any) {
+            ws.send(data);
+        });
+    } else {
+        socketSet.delete(ws);
+    }
+})
 
 // 初始化消息数
 let message = 0;
 
 // 定时2s发送消息
-setInterval(() => {
-    let socketList:any[] = Array.from(socketSet);
-    socketList.forEach(ws => {        
-        if (ws.readyState == 1) {
-            ws.send(JSON.stringify({
-                message: message++,
-                count: socketList.length
-            }))
-        } else {
-            socketSet.delete(ws);
-        }
-    })
-}, 2000)
+// setInterval(() => {
+//     let socketList: any[] = Array.from(socketSet);
+//     socketList.forEach(ws => {
+//         if (ws.readyState == 1) {
+//             ws.send(JSON.stringify({
+//                 message: message++,
+//                 count: socketList.length
+//             }))
+//         } else {
+//             socketSet.delete(ws);
+//         }
+//     })
+// }, 2000)
